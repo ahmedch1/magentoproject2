a456328e637ddf64fe4684601168b59bd99139e5
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-18 12:21:30 +0100

    Add new product attribute

--- app/etc/config.php
+++ app/etc/config.php
@@ -240,6 +240,7 @@ return [
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
+        'Learning_ClothingMaterial' => 1,
         'Learning_FirstUnit' => 1,
         'Learning_GreetingMessage' => 1,
         'Learning_HelloPage' => 1,
